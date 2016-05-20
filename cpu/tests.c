75cfecbe37178fe8d6a57714fdf25aca129802ae
Yubo Su <yubo56@gmail.com>
2016-05-19 21:11:10 -0700

    Vector ODE test

--- cpu/tests.c
+++ cpu/tests.c
@@ -6,11 +6,11 @@
 
 void dydx_exp(float* yn, float* dy, int leny)
 {
-    /* computes dy/dx = y */
+    /* computes dy_k/dx = (-1)^k*y_k */
     int i;
     for (i = 0; i < leny; i++) 
     {
-        dy[i] = yn[i];
+        dy[i] = (i % 2 == 0)? yn[i] : -yn[i];
     }
 }
 
@@ -45,13 +45,41 @@ int main(int argc, const char *argv[])
     ret = solve(dydx_exp, y0, dt, nsteps, leny);
     printf("Final T:%f\tdt:%f\tFinal Value:%f\n", finalT, dt, ret[nsteps]);
     free(ret);
+    printf("True value:%f\n", exp(finalT));
 
+    printf("\nNow testing multiple values!\n");
+    /* run 3 parameters */
+    leny = 2; /* y is length 1 */
+    finalT = 10;
+    free(y0);
+    y0 =  (float*) malloc(leny * sizeof(float));
+    y0[0] = 1;
+    y0[1] = 1;
+
+    /* run 1 parameters */
+    dt = 0.01;
+    nsteps = (int) finalT / dt;
+    /* run solve() */
+    printf("Testing dy/dx = y, Forward Euler\n");
+    ret = solve(dydx_exp, y0, dt, nsteps, leny);
+    printf("Final T:%f\tdt:%f\tFinal Value:(%f, %f)\n", 
+            finalT, dt, ret[2 * nsteps], ret[2 * nsteps + 1]);
+    free(ret);
+
+    /* run 2 parameters */
+    dt = 0.001;
+    nsteps = (int) finalT / dt;
+    /* run solve() */
+    ret = solve(dydx_exp, y0, dt, nsteps, leny);
+    printf("Final T:%f\tdt:%f\tFinal Value:(%f, %f)\n", 
+            finalT, dt, ret[2 * nsteps], ret[2 * nsteps + 1]);
+    free(ret);
     /* 
      * End Forward Euler test code
      * */
 
     /* print true value */
-    printf("True value:%f\n", exp(finalT));
+    printf("True value:(%f, %f)\n", exp(finalT), exp(-finalT));
 
     /* cleanup */
     free(y0);
